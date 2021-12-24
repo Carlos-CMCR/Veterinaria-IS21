df6bd81799a47a4333364a2d7a639414ec4f8ccd
Paul Dc <pdelaguila777@gmail.com>
2021-12-24 10:17:39 -0500

    chat bot

--- view/modulos/inicio.php
+++ view/modulos/inicio.php
@@ -151,6 +151,21 @@
         </div>
 
 
+<!-- CHATBOT -->
+<script>
+  window.watsonAssistantChatOptions = {
+      integrationID: "c148f5ec-f8b6-4b2a-aa01-7f0b491f9523", // The ID of this integration.
+      region: "us-east", // The region your integration is hosted in.
+      serviceInstanceID: "6bdd2c11-a2af-4aee-b47a-8b3d3114cb47", // The ID of your service instance.
+      onLoad: function(instance) { instance.render(); }
+    };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+    document.head.appendChild(t);
+  });
+</script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
